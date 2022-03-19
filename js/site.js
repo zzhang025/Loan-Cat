30ab692b2fe5fa8231335ab78a529e32811df01b
zzhang025 <zhangzx45@gmail.com>
2022-03-19 16:45:21 -0700

    jsupdate

--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
 // Javascript
 
-function getValue(){
+function getValues(){
     // Obtain Inputs from the page
-    let amount = document.getElementById("LoanAmount");
-    let terms = document.getElementById("Terms");
-    let interest = document.getElementById("InterestRate");
+    let amount = document.getElementById("LoanAmount").value;
+    let terms = document.getElementById("Terms").value;
+    let interest = document.getElementById("InterestRate").value;
 
     // Calculate for each parameter
     result = loanCalculator(amount,terms,interest);
@@ -14,12 +14,27 @@ function getValue(){
 }
 
 function loanCalculator(amount,terms,interest){
-    let result={};
+    let result = {
+        month:[],
+        payment:[],
+        interestpaid:[],
+        principal:[],
+        totalinterest=0,
+        balance:[]
+    };
+    let monthly = (amount * (interest/1200))/(1-1/((1+interest/1200)**(terms)));
+    monthly = monthly.toFixed(2);
+    let balance = amount;
     // For loop
     for (let i = 1; i <= terms; i++) {
     result.month.push(i);
+    result.payment.push(monthly);
+    result.interestpaid.push((balance * interest/1200).toFixed(2));
+    result.principal.push((monthly-balance * interest/1200).toFixed(2));
+    result.totalinterest +=(balance * interest/1200).toFixed(2);
+    result.balance.push((balance-(monthly-balance * interest/1200)).toFixed(2));
+    balance = balance-(monthly-balance * interest/1200).toFixed(2); 
 
-    
 
     }
     return result;
