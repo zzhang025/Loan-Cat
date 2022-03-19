90214785413de9be4aad427e188c375a73073f5b
zzhang025 <zhangzx45@gmail.com>
2022-03-19 11:04:30 -0700

    Js.initiate

--- js/site.js
+++ js/site.js
@@ -1,4 +1,26 @@
-//Call hello world
-function Helloworld(){
-    alert("hello world");
+// Javascript
+
+function getValue(){
+    // Obtain Inputs from the page
+    let amount = document.getElementById("LoanAmount");
+    let terms = document.getElementById("Terms");
+    let interest = document.getElementById("InterestRate");
+
+    // Calculate for each parameter
+    result = loanCalculator(amount,terms,interest);
+
+    // Generate Detailed Monthly Payment
+    display(result);
+}
+
+function loanCalculator(amount,terms,interest){
+    let result={};
+    // For loop
+    for (let i = 1; i <= terms; i++) {
+    result.month.push(i);
+
+    
+
+    }
+    return result;
 }
